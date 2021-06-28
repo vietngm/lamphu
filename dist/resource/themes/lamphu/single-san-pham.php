ddace45863fab4caa2d9177cf22ebd60154bdf60
Viet Nguyen Minh <viet.nm@liena.local>
2021-06-28 12:52:04 +0700

    Update
    Update

--- dist/resource/themes/lamphu/single-san-pham.php
+++ dist/resource/themes/lamphu/single-san-pham.php
@@ -14,6 +14,58 @@ $terms = get_term_top_most_parent($terms[0]->term_id, $tax);
         <?php
         if ($mobile_browser == 0) {
           echo '<div class="side">';
+          ?>
+        <div class="nav-cate">
+          <ul class="nav-cate-list">
+            <?php
+		$taxonomy = 'danh-muc';
+		$terms = get_terms(
+			$taxonomy, array(
+			'hide_empty' => 0,
+			'parent' => 0,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			)
+		);
+		foreach($terms as $term){
+			$args = array(
+				"orderby" => "slug",
+				'hide_empty'    => false, 
+				'hierarchical'  => true, 
+				'parent'        => $term->term_id
+			); 
+			$childs = get_terms($taxonomy, $args);
+			$count = count($childs);
+			?>
+            <li class="nav-cate-item">
+              <?php if($count > 0) {?>
+              <a href="#" class="nav-cate-link">
+                <span><?php echo $term->name; ?></span>
+                <span class="arrow arrow-go"></span>
+              </a>
+              <ul class="nav-child">
+                <?php
+            foreach ($childs as $child) {
+                ?>
+                <li class="nav-cate-item">
+                  <a href="<?php echo get_term_link($child->slug, $taxonomy); ?>" class="nav-cate-link">
+                    <span><?php echo $child->name; ?></span>
+                    <span class="arrow arrow-go"></span>
+                  </a>
+                </li>
+                <?php } ?>
+              </ul>
+              <?php }else{ ?>
+              <a href="<?php echo get_term_link($term->slug,$taxonomy);?>" class="nav-cate-link">
+                <span><?php echo $term->name; ?></span>
+                <span class="arrow arrow-go"></span>
+              </a>
+              <?php } ?>
+            </li>
+            <?php } ?>
+          </ul>
+        </div>
+        <?php
           include('sidebar-support.php');
           echo '</div>';
         }
